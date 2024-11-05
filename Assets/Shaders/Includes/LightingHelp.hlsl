1409efe6ad7e3cbb9276cf6eb9d3b49d7bb916d3
christinaqiu3 <christinaqiu3@gmail.com>
2024-11-04 23:27:08 -0500

    wobble added

--- Assets/Shaders/Includes/LightingHelp.hlsl
+++ Assets/Shaders/Includes/LightingHelp.hlsl
@@ -1,17 +1,16 @@
-void GetMainLight_float(float3 WorldPos, out float3 Color, out float3 Direction, out float DistanceAtten, out float ShadowAtten)
-{
+void GetMainLight_float(float3 WorldPos, out float3 Color, out float3 Direction, out float DistanceAtten, out float ShadowAtten) {
 #ifdef SHADERGRAPH_PREVIEW
     Direction = normalize(float3(0.5, 0.5, 0));
     Color = 1;
     DistanceAtten = 1;
     ShadowAtten = 1;
 #else
-#if SHADOWS_SCREEN
+    #if SHADOWS_SCREEN
         float4 clipPos = TransformWorldToClip(WorldPos);
         float4 shadowCoord = ComputeScreenPos(clipPos);
-#else
-    float4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
-#endif
+    #else
+        float4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
+    #endif
 
     Light mainLight = GetMainLight(shadowCoord);
     Direction = mainLight.direction;
@@ -20,6 +19,7 @@ void GetMainLight_float(float3 WorldPos, out float3 Color, out float3 Direction,
     ShadowAtten = mainLight.shadowAttenuation;
 #endif
 }
+
 
 void ComputeAdditionalLighting_float(float3 WorldPosition, float3 WorldNormal,
     float2 Thresholds, float3 RampedDiffuseValues,
@@ -79,13 +79,13 @@ void ComputeAdditionalLighting_float(float3 WorldPosition, float3 WorldNormal,
 #endif
 }
 
-void ChooseColor_float(float3 Highlight, float3 Midtone, float3 Shadow, float Diffuse, float2 Thresholds, out float3 OUT)
+void ChooseColor_float(float3 Highlight, float3 Midtone, float3 Shadow, float Diffuse, float Threshold1, float Threshold2, out float3 OUT)
 {
-    if (Diffuse < Thresholds.x)
+    if (Diffuse < Threshold1)
     {
         OUT = Shadow;
     }
-    else if (Diffuse < Thresholds.y)
+    else if (Diffuse < Threshold2)
     {
         OUT = Midtone;
     }
